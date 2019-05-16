778b7167453c58c22b8a0e548d29abbeafc38b8e
Mehmet Tek <mehmet_tek@windowslive.com>
2019-05-16 21:30:58 +0300

    second commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
-class   MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int number = 0; 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hello"),
+    return Scaffold(
+      appBar:AppBar(title: Text("Refresh Indicator "),),
+      body:new RefreshIndicator(
+       child: Text(number.toString(),style: TextStyle(fontSize: 25.0),),
+       onRefresh: refreshpage, 
+    ), 
+
     );
+  }
+
+  Future<Null> refreshpage() async {
+    await new Future.delayed(new Duration(seconds: 2));
+    setState(() {
+      number +=5;
+    });
+    return null;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:refresh_indicator/homepage.dart';
 
-void main() => runApp(MaterialApp(
-  title:  "Refresh Indicator",
+void main() => runApp(
+  MaterialApp(
+  title: "Refresh Indicator",
   home: MyHomePage(),
-));
+)
+);
 
  
